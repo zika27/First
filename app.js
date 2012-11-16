7c52b1c6414886e9cc9ab847636c2acef3065742
zika27 <zinkina_irina@mail.ru>
2012-11-16 16:25:09 +0200

    deleted

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http'),
-    controller = require('./controller'),
-    model = require('./model');
-
-// Create a new HTTP server
-http.createServer(controller.worker).listen(model.hostPort);
-console.log('Server running at http://'+model.hostName+'/');
